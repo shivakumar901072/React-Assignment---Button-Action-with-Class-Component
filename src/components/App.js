fe273b40f0dfcf9cf5cdca24dbfc635807e78888
shivakumar901072 <shivakumar901072@gmail.com>
2020-11-12 20:46:04 +0530

    react assign classcomponent

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,24 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state ={ 
+			isParaShown: false
+		}
 	};
+	showPara =() => {
+     this.setState({isParaShown: true});
+	}
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				<button id="click">Show Paragraph</button>
+				{this.state.isParaShown ? (
+				<p id="para">
+					 Hello, I've learnt to use the full-stack evaluation tool.
+					  This makes me so happy
+					  </p>
+				) : null}
     		</div>
     	);
     }
